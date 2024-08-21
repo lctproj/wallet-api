72aad2aec45511bbe2f1f3ce159db5e5e6805c6e
LCT <136698906+lctproj@users.noreply.github.com>
2024-08-21 22:18:21 +0800

    refinement

--- server.js
+++ server.js
@@ -1,35 +1,44 @@
 
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
+const fs = require('fs').promises;
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
- 
+app.use(cors());
+
 // Input `npm start server` to start server
 
 
 app.get('/', (req, res) => {
-    res.status(200).json({"message": "Hello, world!"});	
+    res.status(200).json({"message": "server is running"});	
 
 });
 
 app.get('/total-balances/:user_id',async (req, res) => {
     const {user_id} = req.params;
+    console.log('goo');
     try{
-        const response = await fetch('./data.json');
-        const data = await response.json();
+        const response = await fs.readFile('./data.json','utf-8');
+        const data = JSON.parse(response);
 
+        console.log('data = ' + JSON.stringify(data, null, 2));
         const user = data.find((obj) => obj.user_id == user_id);
-        const balance = user.balance;
+        console.log('user = ' + JSON.stringify(user,null,2));
+        if(!user){
+            return res.status(404).json({"message": 'User not found'});
+        }
+        const balances = user.balances;
 
-        res.status(200).json(balance);
-        return
+        return res.status(200).json(balances);
+        
     }catch(err){
-        res.status(500);
-        return
+        return res.status(500);
+        
     }
 
 });
@@ -37,21 +46,28 @@ app.get('/total-balances/:user_id',async (req, res) => {
 app.get('/transfers/:user_id', async (req, res) => {
     const {user_id} = req.params;
 
+    console.log('go');
     try{
-        const response = await fetch('./data.json');
-        const data = await response.json();
-        const user = data.find((obj) => obj.user_id === user_id);
+        const response = await fs.readFile('./data.json','utf-8');
+        const data = JSON.parse(response);
+        console.log('data = ' + JSON.stringify(data, null, 2));
 
-        console.log(user);
+        const user = data.find((obj) => obj.user_id == user_id);
+        console.log('user = ' + JSON.stringify(user,null,2));
 
+ 
         const current_time = new Date();
-        const recent_transfers = user.transfer.filter((transfer) => current_time - Date(transfer.timestamp) <= 8640000);
+        console.log(current_time);
+        const recent_transfers = user.transfers.filter((transfer) => {
+            const transferTime = new Date(transfer.timestamp);
+            return current_time - Date(transfer.timestamp) <= 8640000;
+        });
 
-        res.status(200).json(recent_transfers);
-        return;
+        return res.status(200).json(recent_transfers);
+        ;
     }catch(err){
-        res.status(500);
-        return;
+        return res.status(500);
+        
     }
 });
 
